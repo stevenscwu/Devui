b32052ed1e470b42080da6bdff48d0aacf9dadf8
steven <stevenscwu@gmail.com>
2025-07-28 17:06:30 +0800

    Migrate ignore patterns to eslint.config.js for ESLint v9+

--- eslint.config.js
+++ eslint.config.js
@@ -3,41 +3,59 @@ const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
 
-module.exports = tseslint.config(
-  {
-    files: ["**/*.ts"],
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
-    ],
-    processor: angular.processInlineTemplates,
-    rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
+/**
+ * See: https://eslint.org/docs/latest/use/configure/ignoring-code#the-ignores-configuration-property
+ */
+module.exports = [
+  // TypeScript and Angular rules for .ts files
+  tseslint.config(
+    {
+      files: ["**/*.ts"],
+      extends: [
+        eslint.configs.recommended,
+        ...tseslint.configs.recommended,
+        ...tseslint.configs.stylistic,
+        ...angular.configs.tsRecommended,
       ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
+      processor: angular.processInlineTemplates,
+      rules: {
+        "@angular-eslint/directive-selector": [
+          "error",
+          {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+          },
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+          },
+        ],
+      },
     },
-  },
+    {
+      files: ["**/*.html"],
+      extends: [
+        ...angular.configs.templateRecommended,
+        ...angular.configs.templateAccessibility,
+      ],
+      rules: {},
+    }
+  ),
+  // Global ignores (migrate from .eslintignore)
   {
-    files: ["**/*.html"],
-    extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-    ],
-    rules: {},
+    ignores: [
+      "node_modules/",
+      "dist/",
+      "coverage/",
+      "*.js",
+      "*.d.ts",
+      "*.json"
+      // Add any additional patterns here from your .eslintignore
+    ]
   }
-);
+];
